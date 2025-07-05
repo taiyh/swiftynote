4cceb48bb53d4d2efe07cb692ef9c2e12f6638d6
台源洪 <yuanhong.tai@casstime.com>
2025-07-05 10:15:47 +0800

    update react-native versions

--- ios/Source/UIComponent/Base/BaseNavigationController.swift
+++ ios/Source/UIComponent/Base/BaseNavigationController.swift
@@ -1,33 +1,70 @@
+////
+////  BaseNavigationController.swift
+////  SwiftyNote
+////
+////  Created by taiyh on 2025/1/17.
+////
 //
-//  BaseNavigationController.swift
-//  SwiftyNote
+//import UIKit
 //
-//  Created by taiyh on 2025/1/17.
+//open class BaseNavigationController: UINavigationController {
+//    
+//    
+////    let interactiveTransition = InteractiveTransition()
 //
-
-import UIKit
-
-open class BaseNavigationController: UINavigationController {
-
-    open override func viewDidLoad() {
-        super.viewDidLoad()
-        interactivePopGestureRecognizer?.isEnabled = true
-        interactivePopGestureRecognizer?.delegate = self
-    }
-}
-
-// MARK: status bar style
-extension BaseNavigationController {
-    open override var preferredStatusBarStyle: UIStatusBarStyle {
-        return self.topViewController?.preferredStatusBarStyle ?? .default
-    }
-}
-
-extension BaseNavigationController: UIGestureRecognizerDelegate {
-    open func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        if gestureRecognizer == interactivePopGestureRecognizer {
-            return viewControllers.count > 1
-        }
-        return true
-    }
-}
+//
+//    open override func viewDidLoad() {
+//        super.viewDidLoad()
+////        delegate = self
+//        interactivePopGestureRecognizer?.isEnabled = true
+//        interactivePopGestureRecognizer?.delegate = self
+//        
+//        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+//        pan.delegate = self
+//        view.addGestureRecognizer(pan)
+//    }
+//    
+//    
+//    // MARK: - 手势处理
+//
+//    @objc func handlePan(_ gesture: UIPanGestureRecognizer) {
+//        let translation = gesture.translation(in: view)
+//        let progress = min(max(translation.y / view.bounds.height, 0), 1)
+//
+//        switch gesture.state {
+//        case .began:
+//            if viewControllers.count > 1 {
+//                interactiveTransition.hasStarted = true
+//                popViewController(animated: true)
+//            }
+//        case .changed:
+//            interactiveTransition.update(progress)
+//            interactiveTransition.shouldFinish = progress > 0.3
+//        case .ended, .cancelled:
+//            interactiveTransition.hasStarted = false
+//            if interactiveTransition.shouldFinish {
+//                interactiveTransition.finish()
+//            } else {
+//                interactiveTransition.cancel()
+//            }
+//        default:
+//            break
+//        }
+//    }
+//}
+//
+//// MARK: status bar style
+//extension BaseNavigationController {
+//    open override var preferredStatusBarStyle: UIStatusBarStyle {
+//        return self.topViewController?.preferredStatusBarStyle ?? .default
+//    }
+//}
+//
+//extension BaseNavigationController: UIGestureRecognizerDelegate {
+//    open func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+//        if gestureRecognizer == interactivePopGestureRecognizer {
+//            return viewControllers.count > 1
+//        }
+//        return true
+//    }
+//}
--- ios/SwiftyNote/ViewController.swift
+++ ios/SwiftyNote/ViewController.swift
@@ -7,23 +7,79 @@
 
 import UIKit
 import React
+import Alamofire
+import os.log
 
 class ViewController: BaseViewController {
+    
+    func req() async {
+        // Automatic String to URL conversion, Swift concurrency support, and automatic retry.
+        let response = await AF.request("https://httpbin.org/get", interceptor: .retryPolicy)
+                               // Automatic HTTP Basic Auth.
+                               .authenticate(username: "user", password: "pass")
+                               // Caching customization.
+                               .cacheResponse(using: .cache)
+                               // Redirect customization.
+                               .redirect(using: .follow)
+                               // Validate response code and Content-Type.
+                               .validate()
+                               // Produce a cURL command for the request.
+                               .cURLDescription { description in
+                                 print(description)
+                               }
+                               // Automatic Decodable support with background parsing.
+//                               .serializingDecodable(DecodableType.self)
+                               // Await the full response with metrics and a parsed body.
+                               .response
+        // Detailed response description for easy debugging.
+        debugPrint(response)
+    }
     
     @objc func btttc() {
         let xframe = UIApplication.shared.statusBarFrame
         print("statusBarFrame: \(xframe)")
-        self.navigationController?.pushViewController(RCTBaseViewController(), animated: true)
+        
+        self.present(RCTBaseViewController(), animated: true)
+//        self.navigationController?.pushViewController(RCTBaseViewController(), animated: true)
+        
+        Task {
+            await req()
+        }
+    }
+    
+    @available(iOS 14.0, *)
+    func testlogger() {
+        let logger = Logger()
+        let xxx = 42
+        logger.info("The answer is \(xxx)")
+//        
+//        let logger = OSLog(subsystem: "sub", category: "network")
+//        os_log("Image loaded: %@", log: logger, "imageName")  // 优于 "Image loaded: \(imageName)"
+
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("viewDidLoad")
         statusBarStyle = .darkContent
+        
         navigationItem.title = "ViewController"
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         button.setTitle("button", for: .normal)
         button.addTarget(self, action: #selector(btttc), for: .touchUpInside)
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 25
+        button.layer.maskedCorners = .layerMinXMinYCorner
+//        button.layer.shadowColor
+        button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.06).cgColor
+        button.layer.shadowOffset = CGSize(width: 0, height: 1)
+        button.layer.shadowOpacity = 1
+        button.layer.shadowRadius = 3
+//        button.layer.shadowPath = UIBezierPath(rect: button.bounds).cgPath
+        
+//        button.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner ]
+//        button.layer.cornerRadius = 10
+        
         view.addSubview(button)
 //        let bundleURL = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index");
 //        [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
@@ -39,7 +95,13 @@ class ViewController: BaseViewController {
         
 //        view = rootView
 
-        view.backgroundColor = .lightGray
+//        view.backgroundColor = .lightGray
+        
+        if #available(iOS 14.0, *) {
+            testlogger()
+        } else {
+            // Fallback on earlier versions
+        }
     }
     override func viewWillTransition(
         to size: CGSize,
@@ -67,5 +129,10 @@ class ViewController: BaseViewController {
         super.didReceiveMemoryWarning()
     }
 
+}
+
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, *)
+#Preview {
+    ViewController()
 }
 
